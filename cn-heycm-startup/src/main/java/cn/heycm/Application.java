b74cd8beaa65c349dac9af6bac2b47b71ec8789c
heycm <heycm@qq.com>
2025-07-18 19:30:47 +0800

    first commit

---
+++ cn-heycm-startup/src/main/java/cn/heycm/Application.java
@@ -0,0 +1,17 @@
+package cn.heycm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动模块
+ * @author heycm
+ * @version 1.0
+ * @since 2025/7/18 18:59
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
